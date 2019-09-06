888c269aadcb9043425e78b23ec469ece9fb466f
DarkPitch <david_hall_uk@hotmail.com>
2019-09-06 08:46:25 +0100

    5 files modified, 13 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,9 @@ import {Hero} from './hero';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  heroes = [
-  new Hero(1, 'Windstorm'),
-  new Hero(13, 'Bombasto'),
-  new Hero(15, 'Magneta'),
-  new Hero(20, 'Tornado')
-];
+  
   title = 'Tour of Heroes';
-  myHero = this.heroes[0].name;
-  imgUrl = 'someURL';
+  
+  
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { AppComponent }         from './app.component';
+//import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { MessagesComponent }    from './messages/messages.component';
+
+import { AppRoutingModule }     from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeroesComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    //DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
